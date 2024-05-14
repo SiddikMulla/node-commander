c5cd56a75139838295029fb2b3eae31f4934ab57
sid@747 <sidmm747@gmail.com>
2024-05-14 22:46:01 +0530

    Generating Password

--- Index.js
+++ Index.js
@@ -1,15 +1,23 @@
-const {program}=require('commander')
+const { program } = require('commander')
+const createPassword=require("./utils/createPassword")
+const log = console.log
+program
+    .version('1.1.0')
+    .description("Simple Pass Generator")
+
 
 program
-.version('1.1.0')
-.description("Simple Pass Generator")
+    .option('-l, --length <number>', 'length of password', '8')
+    .option('-s, --save', 'Save as Password.txt')
+    .option('-nn, --no-number', 'remove Numbers')
+    .option('-ns, --no-symbols', 'remove Symbols')
+    .parse()
+
+const { length, save, number, symbols } = program.opts();
+
+//Logic to generate Password
+const generatedPassword = createPassword(length, number, symbols)
 
 
-program
-.option('-l, --length <number>', 'length of password','8')
-.option('-s, --save', 'Save as Password.txt')
-.option('-nn, --no-number', 'remove Numbers')
-.option('-ns, --no-symbols', 'remove Symbols')
-.parse()
-
-console.log(program.opts());
+//Print Generated Password Here
+log(generatedPassword);
