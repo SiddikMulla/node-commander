b35f0aac78c62fa074568205d8561162642ba0f5
sid@747 <sidmm747@gmail.com>
2024-05-16 00:02:01 +0530

    Modify

--- Index.js
+++ Index.js
@@ -2,6 +2,7 @@ import { program } from 'commander'
 import chalk from 'chalk'
 import clipboard from 'clipboardy'
 import createPassword from './utils/createPassword.js'
+import savePassword from './utils/savePassword.js'
 const log = console.log
 
 program
@@ -21,10 +22,14 @@ const { length, save, number, symbols } = program.opts();
 //Logic to generate Password
 const generatedPassword = createPassword(length, number, symbols)
 
+//Save Password to .txt
+if (save) {
+    savePassword(generatedPassword)
+}
 //Copy to Clipboard
 clipboard.writeSync(generatedPassword);
 
 //Print Generated Password Here
-log(chalk.yellow(chalk.bold("Generated Password: "))+chalk.red(chalk.bold(generatedPassword)));
-    
+log(chalk.yellow(chalk.bold("Generated Password: ")) + chalk.red(chalk.bold(generatedPassword)));
+
 log(chalk.green("Copied to Clipboard"));
