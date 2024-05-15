ab2789f0dff88da0e50fd8dcf1c04d6db33006b0
sid@747 <sidmm747@gmail.com>
2024-05-15 23:56:57 +0530

    file system added

---
+++ utils/savePassword.js
@@ -0,0 +1,16 @@
+import fs from "fs"
+import path from "path"
+import os from "os"
+
+import chalk from "chalk"
+
+const savePassword = (password) => {
+    fs.open(path.join(__dirname, "../", "password.txt"), "a", 666, (e, id) => {
+        fs.write(id, password + os.EOL, null, "utf-8", () => {
+            fs.close(id, () => {
+                console.log(chalk.yellow("password Saved to Password.txt"))
+            })
+        })
+    })
+}
+export default savePassword;
